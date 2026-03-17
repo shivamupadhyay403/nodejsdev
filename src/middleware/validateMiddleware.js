7b3e896fe357eb706a5258dac401738d08c5d67f
shivamu1818 <shivam.gazaniainfotech.com>
2026-03-17 20:28:05 +0530

    added error structures of joi for login and register respectively

---
+++ src/middleware/validateMiddleware.js
@@ -0,0 +1,24 @@
+const validate = (schema, property = 'body') => {
+    return (req, res, next) => {
+      const { error, value } = schema.validate(req[property], {
+        abortEarly: false, // show all errors
+        allowUnknown: false, // reject extra fields
+        stripUnknown: true, // remove unwanted fields
+      });
+  
+      if (error) {
+        return res.status(400).json({
+          message: 'Validation error',
+          errors: error.details.map(err => ({
+            field: err.path.join('.'),
+            message: err.message,
+          })),
+        });
+      }
+  
+      req[property] = value; // sanitized data
+      next();
+    };
+  };
+  
+  module.exports = validate;
