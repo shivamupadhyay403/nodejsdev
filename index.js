bfcd280957d2d4f61994ab9ca3f1820aaa505b5e
shivamu1818 <shivam.gazaniainfotech.com>
2026-03-18 19:44:09 +0530

    Added mongo db connection

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const express = require("express");
 const dotenv = require("dotenv");
 const app = express();
 const users = require("./src/routes/userRoute");
+const handleConnecttoDb = require("./src/db/connectDb");
 dotenv.config();
+handleConnecttoDb()
+  .then((res) => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(process.env.PORT, () => {
   console.log("Hello Application running on port ", process.env.PORT);
---
+++ src/db/connectDb.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const handleConnecttoDb = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URI);
+  } catch (err) {
+    console.log(err);
+  }
+};
+module.exports = handleConnecttoDb;
