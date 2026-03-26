f83f5c404c53a8eab9da336554f37bd7809610bb
shivamu1818 <shivam.gazaniainfotech.com>
2026-03-26 23:20:01 +0530

    added api

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,10 @@
-export const loginUser=(req,res)=>{
-    return res.json({messsage:"Hello from Login APi Build By Shivam"})
-}
+export const loginUser = (req, res) => {
+  return res.json({ messsage: "Hello from Login APi" });
+};
 
-export const registerUser=(req,res)=>{
-    return res.json({messsage:"Hello from Login APi"})
-}
+export const registerUser = (req, res) => {
+  return res.json({ messsage: "Hello from Login APi" });
+};
+export const helloUser = (req, res) => {
+  return res.json({ messsage: "Hello By Shivam" });
+};
